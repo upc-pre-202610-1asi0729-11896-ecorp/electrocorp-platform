5c16a6ef2a29d99b742c082d53487f8a2a62b829
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-17 17:22:06 -0500

    Update DevicePowerPolicyService.java

--- src/main/java/com/electrocorp/electrocorpplatform/devicecontrol/domain/services/DevicePowerPolicyService.java
+++ src/main/java/com/electrocorp/electrocorpplatform/devicecontrol/domain/services/DevicePowerPolicyService.java
@@ -15,7 +15,16 @@ public class DevicePowerPolicyService {
 
     public String resolvePowerCategory(BigDecimal powerWatts) {
         if (powerWatts == null) {
-            return "UNKNOWN
+            return "UNKNOWN";
+        }
+
+        if (powerWatts.compareTo(BigDecimal.valueOf(100)) < 0) {
+            return "LOW";
+        }
+
+        if (powerWatts.compareTo(HIGH_POWER_THRESHOLD) < 0) {
+            return "MEDIUM";
+        }
 
         return "HIGH";
     }
